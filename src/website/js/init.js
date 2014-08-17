8a8cd1f843a547fcc84d0da07897f08f3ce32603
Alex Lindgren <alindgren@flightpath.com>
2014-08-16 21:43:12 -0400

    working on templates

--- src/website/js/init.js
+++ src/website/js/init.js
@@ -9,12 +9,12 @@
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			global:		{ range: '*', href: 'css/style.css', containers: 1400, grid: { gutters: 50 } },
-			wide:		{ range: '-1680', href: 'css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
-			normal:		{ range: '-1280', href: 'css/style-normal.css', containers: 960, lockViewport: true },
-			narrow:		{ range: '-980', href: 'css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
-			narrower:	{ range: '-840', href: 'css/style-narrower.css', grid: { collapse: 1 } },
-			mobile:		{ range: '-640', href: 'css/style-mobile.css', containers: '100%', grid: { gutters: 15, collapse: 2 } }
+			global:		{ range: '*', href: '/css/style.css', containers: 1400, grid: { gutters: 50 } },
+			wide:		{ range: '-1680', href: '/css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
+			normal:		{ range: '-1280', href: '/css/style-normal.css', containers: 960, lockViewport: true },
+			narrow:		{ range: '-980', href: '/css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
+			narrower:	{ range: '-840', href: '/css/style-narrower.css', grid: { collapse: 1 } },
+			mobile:		{ range: '-640', href: '/css/style-mobile.css', containers: '100%', grid: { gutters: 15, collapse: 2 } }
 		}
 	}, {
 		layers: {
