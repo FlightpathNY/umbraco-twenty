7318d02a752add921128732e14429b47a3815a06
Alex Lindgren <alindgren@flightpath.com>
2014-08-19 15:23:57 -0400

    Article doc type and leftsidebar template

---
+++ src/website/Controllers/ArticleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Zone.UmbracoMapper;
+using UmbracoTwenty.Models;
+
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoTwenty.Controllers
+{
+    public class ArticleController : BaseController
+    {
+        public ArticleController(IUmbracoMapper mapper)
+            : base(mapper)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            ArticleViewModel model = new ArticleViewModel();
+            Mapper.Map(CurrentPage, model);
+            MapFooter(model);
+
+            return CurrentTemplate(model);
+        }
+    }
+}
---
+++ src/website/Models/ArticleViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using Zone.UmbracoMapper;
+
+namespace UmbracoTwenty.Models
+{
+    public class ArticleViewModel : BaseViewModel
+    {
+        [PropertyMapping(SourceProperty = "Title")]
+        public string Title { get; set; }
+
+        [PropertyMapping(SourceProperty = "Subtitle")]
+        public string Subtitle { get; set; }
+
+        [PropertyMapping(SourceProperty = "FeaturedImage")]
+        public string FeaturedImage { get; set; }
+
+        [PropertyMapping(SourceProperty = "Header")]
+        public string Header { get; set; }
+
+        [PropertyMapping(SourceProperty = "Body")]
+        public string Body { get; set; }
+
+    }
+}
